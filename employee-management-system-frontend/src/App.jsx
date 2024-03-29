75ecaaf0ee22f3eeae85d7b62864e7f9c8cc647e
Kasthuri <kasthuri93441@gmail.com>
2024-03-29 15:33:22 +0530

    ems-frontend

---
+++ employee-management-system-frontend/src/App.jsx
@@ -0,0 +1,27 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import './App.css'
+import HelloWorld from './HelloWorld'
+import Footer from './components/Footer'
+import Header from './components/Header'
+import ListEmployee from './components/ListEmployee'
+import Employee from './components/Employee'
+
+function App() {
+ 
+  return (
+    <>
+      <BrowserRouter>
+      <Header/>
+      <Routes>
+        <Route path='/' element={<ListEmployee/>}></Route>
+        <Route path='/employees' element={<ListEmployee/>}></Route>
+        <Route path='/add-employee' element={<Employee/>}></Route>
+        <Route path='/edit-employee/:id' element={<Employee/>}></Route>
+      </Routes>
+      <Footer/>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
